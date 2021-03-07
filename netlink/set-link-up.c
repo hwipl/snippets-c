eb137328c9c59834ce68d0b6528e08edeb7574cc
hwipl <33433250+hwipl@users.noreply.github.com>
2021-03-07 17:54:31 +0100

    add netlink/set-link-up.c
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ netlink/set-link-up.c
@@ -0,0 +1,75 @@
+/* netlink imports */
+#include <asm/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <linux/rtnetlink.h>
+
+/* memset */
+#include <string.h>
+
+/* printf */
+#include <stdio.h>
+
+/* if_nametoindex */
+#include <net/if.h>
+
+/* create netlink socket and return socket fd */
+int create_socket() {
+	/* create socket address */
+	struct sockaddr_nl sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.nl_family = AF_NETLINK;
+
+	/* create and bind socket */
+	int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+	if (fd < 0) {
+		return fd;
+	}
+	if (bind(fd, (struct sockaddr *) &sa, sizeof(sa))) {
+		return -1;
+	}
+	return fd;
+}
+
+/* send request to get all links */
+int send_request(int fd, const char *if_name) {
+	/* create socket address */
+	struct sockaddr_nl sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.nl_family = AF_NETLINK;
+
+	/* create request message */
+	struct {
+		struct nlmsghdr hdr;
+		struct ifinfomsg ifi;
+	} req;
+	memset(&req, 0, sizeof(req));
+
+	/* fill request header */
+	req.hdr.nlmsg_len = NLMSG_LENGTH(sizeof(req.ifi));
+	req.hdr.nlmsg_pid = 0;
+	req.hdr.nlmsg_seq = 1;
+	req.hdr.nlmsg_type = RTM_SETLINK;
+	req.hdr.nlmsg_flags = NLM_F_REQUEST;
+
+	/* fill request interface info */
+	req.ifi.ifi_family = AF_UNSPEC;
+	req.ifi.ifi_index = if_nametoindex(if_name);
+	req.ifi.ifi_change = 0xffffffff;
+	req.ifi.ifi_flags |= IFF_UP;
+
+	/* send request */
+	struct iovec iov = { &req, req.hdr.nlmsg_len };
+	struct msghdr msg = { &sa, sizeof(sa), &iov, 1, NULL, 0, 0 };
+	sendmsg(fd, &msg, 0);
+
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		return -1;
+	}
+	int fd = create_socket();
+	send_request(fd, argv[1]);
+}
